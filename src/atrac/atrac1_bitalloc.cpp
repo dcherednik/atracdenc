4be2e387a494b1c02d2ca0747dd64b6ba8980d21
Daniil Cherednik <dan.cherednik@gmail.com>
2024-11-03 00:44:19 +0100

    Reduce hearable noise in some tracks for ATRAC1 encoder.
    https://hydrogenaud.io/index.php/topic,120193.0.html
    
    ATH + changes of bitallocation table allow significant reduce the noise. But some artefacts still present.

--- src/atrac/atrac1_bitalloc.cpp
+++ src/atrac/atrac1_bitalloc.cpp
@@ -34,9 +34,9 @@ using std::endl;
 using std::pair;
 
 static const uint32_t FixedBitAllocTableLong[TAtrac1BitStreamWriter::MaxBfus] = {
-    7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 5, 5, 5, 5, 5, 5, 5, 5, 6,
+    7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
     6, 6, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4,
-    4, 4, 3, 3, 3, 3, 3, 3, 2, 2, 2, 1, 1, 0, 0, 0
+    4, 4, 3, 3, 3, 3, 3, 3, 2, 1, 1, 1, 1, 0, 0, 0
 };
 
 static const uint32_t FixedBitAllocTableShort[TAtrac1BitStreamWriter::MaxBfus] = {
